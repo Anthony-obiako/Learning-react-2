0aeb7095112dff07f9acfeab9b8d695d327deb15
Anthony Obiako <anthonyobiakoc@gmail.com>
2023-10-14 15:33:16 +0100

    created a meme generator

---
+++ react-app-2/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Meme from "./components/Form";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app-2/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h2 className="header-title">Meme Generator</h2>
+      <h4 className="header-project">React Course - Project 3</h4>
+    </header>
+  );
+}
+
+export default Header;
